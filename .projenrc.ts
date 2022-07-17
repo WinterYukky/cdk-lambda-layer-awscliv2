e6517e7a9bf1ee70d3e6e2a5e6f335bc7e1201b1
WinterYukky <49480575+WinterYukky@users.noreply.github.com>
2022-07-17 08:23:49 +0000

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,18 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'WinterYukky',
+  authorAddress: '49480575+WinterYukky@users.noreply.github.com',
+  cdkVersion: '2.32.1',
+  defaultReleaseBranch: 'main',
+  name: 'cdk-lambda-layer-awscliv2',
+  projenrcTs: true,
+  prettier: true,
+  eslintOptions: { dirs: ['src'], prettier: true },
+  repositoryUrl: 'https://github.com/WinterYukky/cdk-lambda-layer-awscliv2.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
